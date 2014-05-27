f2a6e7c98dff2d07f641efc809f2299db716b5d9
Jannis Grimm <jannis@gje.ch>
2014-05-27 20:30:51 +0200

    Add form

--- src/ch/hsr/challp/neo2/HashCollisionServlet.java
+++ src/ch/hsr/challp/neo2/HashCollisionServlet.java
@@ -35,36 +35,14 @@ public class HashCollisionServlet extends HttpServlet {
     writer.println("<html>");
     writer.println("<head><title>Hello World Servlet</title></head>");
     writer.println("<body>");
-
-    HashCollision hashCollision = new HashCollision();
-    
-    for (int iterations = 1000; iterations < 100000; iterations += 1000) {
-      System.out.print((iterations / 1000.0) + "k iterations: ");
-      
-      long start = System.currentTimeMillis();
-      for (int i = 0; i < iterations; i++) {
-        StringBuilder sb = new StringBuilder();
-        for (int n = 0; n < i; n++) {
-          sb.append("0");
-        }
-        sb.append("a");
-        hashCollision.addUser(sb.toString());
-        sb = null;
-      }
-      System.out.print(((System.currentTimeMillis() - start) / (iterations / 100.0)) + "ms/");
-  
-      long startHack = System.currentTimeMillis();
-      for (int i = 0; i < iterations; i++) {
-        StringBuilder sb = new StringBuilder();
-        for (int n = 0; n < i; n++) {
-          sb.append("\0");
-        }
-        sb.append("a");
-        hashCollision.addUser(sb.toString());
-        sb = null;
-      }
-      System.out.println(((System.currentTimeMillis() - startHack) / (iterations / 100.0)) + "ms");
-    }
+    writer.println("<h1>Registrieren</h1>");
+    writer.println("<form method='post'>");
+    writer.println("<label>");
+    writer.println("Username:");
+    writer.println("<input name='name'>");
+    writer.println("</label>");
+    writer.println("<input type='submit'>");
+    writer.println("</form>");
     writer.println("<body>");
     writer.println("</html>");
 
@@ -76,8 +54,49 @@ public class HashCollisionServlet extends HttpServlet {
    *      response)
    */
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
-      throws ServletException, IOException {
-    // TODO Auto-generated method stub
+      throws ServletException, IOException {HashCollision hashCollision = new HashCollision();
+
+      PrintWriter writer = response.getWriter();
+
+      writer.println("<html>");
+      writer.println("<head><title>Hello World Servlet</title></head>");
+      writer.println("<body>");
+      writer.println("<h1>Erfolgreich</h1>");
+      writer.println("Hallo");
+      writer.println(request.getParameter("name"));
+      writer.println("</form>");
+      writer.println("<body>");
+      writer.println("</html>");
+
+      writer.close();
+      
+      for (int iterations = 1000; iterations < 100000; iterations += 1000) {
+        System.out.print((iterations / 1000.0) + "k iterations: ");
+        
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < iterations; i++) {
+          StringBuilder sb = new StringBuilder();
+          for (int n = 0; n < i; n++) {
+            sb.append("0");
+          }
+          sb.append("a");
+          hashCollision.addUser(sb.toString());
+          sb = null;
+        }
+        System.out.print(((System.currentTimeMillis() - start) / (iterations / 100.0)) + "ms/");
+    
+        long startHack = System.currentTimeMillis();
+        for (int i = 0; i < iterations; i++) {
+          StringBuilder sb = new StringBuilder();
+          for (int n = 0; n < i; n++) {
+            sb.append("\0");
+          }
+          sb.append("a");
+          hashCollision.addUser(sb.toString());
+          sb = null;
+        }
+        System.out.println(((System.currentTimeMillis() - startHack) / (iterations / 100.0)) + "ms");
+      }
   }
 
 }
