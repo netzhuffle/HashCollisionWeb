4e854c58d38a2a5389e517211d5c2c4a7c02d251
Jannis Grimm <jannis@gje.ch>
2014-06-01 21:34:05 +0200

    Add hash debugging

--- src/ch/hsr/challp/neo2/Hacker.java
+++ src/ch/hsr/challp/neo2/Hacker.java
@@ -1,13 +1,22 @@
 package ch.hsr.challp.neo2;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
+//import sun.misc.Hashing;
 
 public class Hacker {
 
   public static void main(String[] args) throws Exception {
+//    String a = "a";
+//    String na = "\0a";
+//    String nnna = "\0\0\0a";
+//    String an = "a\0";
+//    System.out.println(a.hashCode() + "/" + na.hashCode() + "/" + nnna.hashCode() + "/" + an.hashCode());
+//    System.out.println(Hashing.stringHash32(a) + "/" + Hashing.stringHash32(na) + "/" + Hashing.stringHash32(nnna) + "/" + Hashing.stringHash32(an));
+
     URL server = new URL(
         "http://localhost:8080/HashCollisionWeb/HashCollisionServlet");
     HttpURLConnection conn = (HttpURLConnection) server.openConnection();
